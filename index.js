237c2362e2e265b1f943122066d4a5e96a668621
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2024-07-01 20:40:06 +0200

    Fixed fullscreen image navigation

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
 // IMGS in fullscreen
-function getPics() {}
-const test = document.getElementsByClassName("test-imgs");
+
 const imgs = document.querySelectorAll("#imgs div img");
 const fullPage = document.querySelector("#fullpage");
 
-imgs.forEach((img) => {
+let currentIndex = 0;
+
+function showImage(index) {
+  currentIndex = index;
+  fullPage.style.backgroundImage = "url(" + imgs[index].src + ")";
+  fullPage.style.display = "block";
+}
+
+imgs.forEach((img, index) => {
   img.addEventListener("click", function () {
-    fullPage.style.backgroundImage = "url(" + img.src + ")";
-    fullPage.style.display = "block";
+    showImage(index);
   });
 });
-console.log(test);
 
-// Hamburger
+document.addEventListener("keydown", function (e) {
+  if (fullPage.style.display === "block") {
+    if (e.key === "ArrowLeft") {
+      currentIndex = (currentIndex - 1 + imgs.length) % imgs.length;
+      showImage(currentIndex);
+    } else if (e.key === "ArrowRight") {
+      currentIndex = (currentIndex + 1) % imgs.length;
+      showImage(currentIndex);
+    } else if (e.key === "Escape") {
+      fullPage.style.display = "none";
+    }
+  }
+});
+
+fullPage.addEventListener("click", function (e) {
+  if (e.target === fullPage) {
+    fullPage.style.display = "none";
+  }
+});
+
+let startX;
+fullPage.addEventListener("touchstart", function (e) {
+  startX = e.touches[0].clientX;
+});
+
+fullPage.addEventListener("touchend", function (e) {
+  let endX = e.changedTouches[0].clientX;
+  if (startX > endX + 50) {
+    currentIndex = (currentIndex + 1) % imgs.length;
+    showImage(currentIndex);
+  } else if (startX < endX - 50) {
+    currentIndex = (currentIndex - 1 + imgs.length) % imgs.length;
+    showImage(currentIndex);
+  }
+});
